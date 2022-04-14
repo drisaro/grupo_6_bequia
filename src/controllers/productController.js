a37095f92f304eb82bbbac1b9520aa8cfa17f32d
raimercm <raimercm@gmail.com>
2022-04-14 11:22:32 -0500

    agregando html y  css products, dada.json y funcionalidad

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 const controller = {
+    index: (req, res) => {
+        let productList=products.filter(product=>product.show)
+        res.render("products",{productList,toThousand})
+    },
     createProduct: (req, res) => {
         res.render("createProduct")
     },
@@ -9,7 +21,9 @@ const controller = {
         res.render("carrito")
     },
     product: (req, res) => {
-        res.render("productDetail")
+        let idProduct=req.params.id;
+        let product=products.find(product=>product.id==idProduct)
+        res.render("productDetail", {product,toThousand})
     },
     storeProduct: (req,res) => {
         console.log("Producto guardado") //Falta generar lógica para redirigir los datos (guardar y mostrar la vista) utilizar res.redirect 
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
+router.get("/", productController.index);
 router.get("/createProduct", productController.createProduct);
 router.post("/createProduct", productController.storeProduct);
 
